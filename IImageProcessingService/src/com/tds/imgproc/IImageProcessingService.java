177cfe2e0e666372ceff2228bf0a50ae4659b896
Ragmo <ragmoarno@yahoo.com>
2014-12-10 10:58:25 +0100

    objectdetect added convert given bufferedimages to mat saves images as jpg

--- IImageProcessingService/src/com/tds/imgproc/IImageProcessingService.java
+++ IImageProcessingService/src/com/tds/imgproc/IImageProcessingService.java
@@ -1,3 +1,35 @@
+<<<<<<< HEAD
+/**
+ * 
+ */
+package com.tds.imgproc;
+
+import java.awt.image.BufferedImage;
+import java.util.Enumeration;
+
+import org.opencv.core.Mat;
+
+/**
+ * <b>IImageProcessingService <br />
+ * com.tds.imgproc <br />
+ * IImageProcessingService <br />
+ * </b>
+ *
+ * Description.
+ *
+ * @author Phillip Kopprasch<phillip.kopprasch@gmail.com>
+ * @created 12.11.2014 20:33:30
+ *
+ */
+public interface IImageProcessingService {
+
+	 public void detectSleep(BufferedImage inputframe);
+	 public BufferedImage getbufferedImgae(Mat frame);
+	 public Enumeration getType();
+	 public void detectObject(BufferedImage input, BufferedImage compare);
+	
+}
+=======
 /**
  *
  */
@@ -22,3 +54,4 @@ public interface IImageProcessingService {
     public Mat detect(Mat inputframe);
 
 }
+>>>>>>> 90ddb0f23332b23e27af5a98253a141fff894fd8
