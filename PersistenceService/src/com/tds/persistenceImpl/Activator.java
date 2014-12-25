15914ac05855e79d7d5365e8909d1222dadf4843
Phillip Kopprasch <phillip.kopprasch@gmail.com>
2014-12-25 12:33:04 +0100

    implemeneted the activator of the persistence service

--- PersistenceService/src/com/tds/persistenceImpl/Activator.java
+++ PersistenceService/src/com/tds/persistenceImpl/Activator.java
@@ -1,11 +1,18 @@
 package com.tds.persistenceImpl;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+
+import com.tds.persistence.IPersistenceService;
 
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
+	private IPersistenceService service;
 
 	static BundleContext getContext() {
 		return context;
@@ -17,6 +24,11 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+        service = new PersistenceService();
+        Dictionary<String, Object> params = new Hashtable<>();
+        params.put(Constants.SERVICE_PID, IPersistenceService.class.getName());
+        params.put(Constants.SERVICE_DESCRIPTION, "Provides access to the persistence layer of the application.");
+        context.registerService(IPersistenceService.class.getName(), service, params);
 	}
 
 	/*
