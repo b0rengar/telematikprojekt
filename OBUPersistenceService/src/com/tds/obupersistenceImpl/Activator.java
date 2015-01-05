cd9ab9c1ee1b348a623dc731739669e3e4a141af
Andre <andrefinsterbusch@googlemail.de>
2015-01-05 13:55:32 +0100

    some fixes

--- OBUPersistenceService/src/com/tds/obupersistenceImpl/Activator.java
+++ OBUPersistenceService/src/com/tds/obupersistenceImpl/Activator.java
@@ -11,32 +11,36 @@ import com.tds.obupersistence.IOBUPersistenceService;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
-	private IOBUPersistenceService service;
+    private static BundleContext context;
+    private IOBUPersistenceService service;
 
-	static BundleContext getContext() {
-		return context;
-	}
+    static BundleContext getContext() {
+        return context;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        Activator.context = bundleContext;
         service = new OBUPersistenceService();
         Dictionary<String, Object> params = new Hashtable<>();
         params.put(Constants.SERVICE_PID, IOBUPersistenceService.class.getName());
         params.put(Constants.SERVICE_DESCRIPTION, "Provides access to the persistence layer of the application on the OBU.");
         context.registerService(IOBUPersistenceService.class.getName(), service, params);
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        Activator.context = null;
+    }
 
 }
